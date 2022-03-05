f8ff617946c46683f1928721839630405b51f3fb
MohamadSyahrul <mochamadsahrul28@gmail.com>
2022-03-05 14:26:45 +0700

    update response

--- method.php
+++ method.php
@@ -10,18 +10,28 @@ class barang
       $query="SELECT tanggal, namacust, penjorderdet.barangid, penjorderdet.qty, penjorderdet.harga FROM penjorder 
             left join penjorderdet on penjorder.noorder = penjorderdet.noorder
             left join barang on penjorderdet.barangid = barang.barangid";
-            
+                   
       $data=array();
       $result=$mysqli->query($query);
       while($row=mysqli_fetch_object($result))
       {
          $data[]=$row;
       }
-      $response=array(
+          if($result)
+            {
+               $response=array(
                      'status' => 1,
                      'message' =>'Order Successs',
                      'data' => $data
                   );
+            }
+            else
+            {
+               $response=array(
+                  'status' => 0,
+                  'message' =>'Order Failed'
+               );
+            }
       header('Content-Type: application/json');
       echo json_encode($response);
    }
